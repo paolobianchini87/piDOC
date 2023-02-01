83c7d91c980f2802795ccf977b073f27f7f8aa01
Paolo Bianchini <90002009+paolobianchini87@users.noreply.github.com>
2023-02-01 15:48:28 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'π-DOC'
+copyright = '2023, Paolo Bianchini'
+author = 'Paolo Bianchini'
 
 release = '0.1'
 version = '0.1.0'
